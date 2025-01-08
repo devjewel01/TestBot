04f490e84d2ecd47c8121517462cc39b0d55040d
Jewel Nath <jewel.nath@orbitax.com>
2025-01-08 19:03:43 +0600

    init testbot_driver

---
+++ src/testbot_driver/setup.py
@@ -0,0 +1,35 @@
+# setup.py
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'testbot_driver'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include config files
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='orbitax',
+    maintainer_email='jewel.nath@orbitax.com',
+    description='TODO: Package description',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hardware_interface = testbot_driver.hardware_interface:main',
+        ],
+    },
+)
